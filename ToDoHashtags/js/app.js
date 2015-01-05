36d29a02a9cdc400cb289ab134c8cbcb0034842b
joetime <joetime@gmail.com>
2015-01-05 10:25:20 -0500

    for some reason this didn't come with the rest... ?

---
+++ ToDoHashtags/js/app.js
@@ -0,0 +1,54 @@
+﻿(function () {
+
+    var app = angular.module('todos', []);
+
+    app.controller('TodosController', function () {
+        this.myTodos = JSON.parse(localStorage.getItem('todos')) || [];
+
+        //Add New ToDo
+        this.addTodo = function () {
+            var newtodotext = document.getElementById("todotext").value;
+            var numTags = document.getElementsByClassName('todotag').length;
+            var newTags = [];
+            if (numTags > 0) {
+                for (var i = 0; i < numTags; i++) {
+                    newTags[i] = document.getElementById("todotag" + (i + 1)).value;
+                }
+            }
+
+            this.myTodos.push({
+                'todotext': newtodotext,
+                'tags': newTags
+            });
+            // Clean Form
+            document.forms["todoForm"].reset();
+
+            // Save New Todo TO Local Storage
+            localStorage.setItem('todos', JSON.stringify(this.myTodos));
+        };
+
+        //Delete slected todo
+        this.deleteTodo = function (todo) {
+            this.myTodos.splice(this.myTodos.indexOf(todo), 1);
+            localStorage.setItem('todos', JSON.stringify(this.myTodos));
+        };
+
+        this.showTagTodos = function (tag) {
+            this.tagTodos = [];
+            for (var i = 0; i < this.myTodos.length; i++) {
+                if (this.myTodos[i].tags == tag) {
+                    this.tagTodos.push(this.myTodos[i]);
+                }
+            }
+            var tester = [];
+            for (var i = 0; i < this.tagTodos.length; i++) {
+                tester[i] = this.tagTodos[i].todotext;
+            }
+            alert(tester.toString());
+        };
+
+    });
+
+
+
+})();
