8eefc9db645f091818f59dd88f139c994d2bfac9
joetime <joetime@gmail.com>
2015-01-05 10:11:06 -0500

    first commit

---
+++ ToDoHashtags/js/custom-jquery.js
@@ -0,0 +1,7 @@
+﻿jQuery( document ).ready(function($) {	
+    var numtags = 1;
+    $("#addTag").click(function () {
+        $("#tags").append('<textarea id="todotag' + numtags + '" class="todotag" rows="1" cols="20"></textarea>');
+        numtags = numtags + 1;
+    });
+});
